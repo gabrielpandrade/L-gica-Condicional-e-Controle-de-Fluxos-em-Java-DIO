3a28383546f804c7f3a2e47184ef380eaa538fa4
gabrielpandrade <pintoandradegabriel@gmail.com>
2022-02-20 21:55:54 -0300

    first commit

---
+++ src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*
+        * Operadores relacionais:
+        * == -> Igualdade
+        * != -> Diferença
+        * >, >= -> Maior e maior ou igual
+        * <, <= -> Menor e menor ou igual
+        * */
+
+        Exercicio01.operadoresRelacionias();
+    }
+}
